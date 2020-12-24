5e9913a83ba96a10bfdcb1c577369c9a593e9dce
Lucas César <lucasambrogi1@hotmail.com>
2020-12-23 23:59:17 -0300

    Main page: start of the page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div id="container" >
+      <header>
+        <div className="header-text" >
+          <h1>Enshort, expand or create a custom URL!</h1>
+
+          <p>Convert an URL to a giant or tiny one!</p>
+        </div>
+      </header>
+
+      <main>
+        <form>
+          <fieldset>
+            <h2>Original URL</h2>
+
+            <input type="text" />
+
+            <h2>Custom name</h2>
+
+            <input type="text" />
+          </fieldset>
+
+          <button type="submit">
+            <h1>Create URL</h1>
+          </button>
+        </form>
+      </main>
+    </div>
   )
 }
